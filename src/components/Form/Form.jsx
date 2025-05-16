a003a80d9d965b87a070f0e824b6a39ae244fcc3
Agustin Macko <mackoaa@gmail.com>
2025-05-16 14:19:22 -0300

    fix form desktop

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,12 +5,12 @@ import Select from "../Select/Select";
 import style from "./Form.module.css";
 import { provinciasArgentinas} from "../../data/province";
 import Textarea from "../Textarea/Textarea";
+import useIsMobile from "../../hook/useIsMobile";
 
 
 const Form = () => {
   const [formData, setFormData] = useState({
     name: "",
-    lastName: "",
     cellphone: "",
     email: "",
     province: ""
@@ -25,35 +25,30 @@ const Form = () => {
     console.log("Submitted:", formData);
   };
 
-  return (
-    <form onSubmit={handleSubmit} className= {style["form-wraper"]} >
+  const isMobile = useIsMobile();
+
+ return isMobile ? (
+    <form onSubmit={handleSubmit} className={style["form-wraper"]}>
       <Input
-        label="Nombre"
+        label="Nombre y apellido"
         name="name"
         value={formData.name}
         onChange={handleChange}
-        placeholder="Ingresá tu nombre"
+        placeholder="Ingresá nombre y apellido"
       />
-      <Input
-        label="Apellido"
-        name="lastName"
-        value={formData.lastName}
-        onChange={handleChange}
-        placeholder="Ingresá tu apellido"
-      />
+        <Input
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="nombre@mail.com"
+        />
       <Input
         label="Teléfono/celular"
         name="cellphone"
         value={formData.cellphone}
         onChange={handleChange}
         placeholder="Nro. de teléfono (sin el 15)"
-      />
-      <Input
-        label="Email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        placeholder="nombre@mail.com"
       />
       <Select
         label="Provincia"
@@ -78,13 +73,80 @@ const Form = () => {
         </label>
       </div>
 
+      <div className={style.buttonWrapper}>
+        <Button type="submit" variant="primary">
+          Enviar consulta
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            strokeWidth={2.4}
+            stroke="currentColor"
+            fill="none"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5L15.75 12 8.25 19.5"
+            />
+          </svg>
+        </Button>
+      </div>
+    </form>
+  ) : (
+    <form  onSubmit={handleSubmit} className={style["form-wraper"]}>
+      <div className={style.wraperInput}> 
+        <Input
+        label="Nombre y apellido"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        placeholder="Ingresá nombre y apellido"
+      />
+        <Input
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="nombre@mail.com"
+        />
+      </div>
+
+      <div className={style.wraperInput}>
+        <Input
+        label="Teléfono/celular"
+        name="cellphone"
+        value={formData.cellphone}
+        onChange={handleChange}
+        placeholder="Nro. de teléfono (sin el 15)"
+      />
+      <Select
+        label="Provincia"
+        name="province"
+        value={formData.province}
+        onChange={handleChange}
+        options={provinciasArgentinas}
+        placeholder="Seleccioná tu provincia"
+      />
+      </div>
+      <Textarea
+        label="Mensaje"
+        name="message"
+        value={formData.message}
+        onChange={handleChange}
+        placeholder="Escribí tu mensaje"
+      />
+      <div className={style.checkboxWrapper}>
+        <label className={style.customCheckbox}>
+          <input type="checkbox" name="subscribe" />
+          <span className={style.checkmark}></span>
+          Acepto recibir información y novedades en mi email.
+        </label>
+      </div>
 
       <div className={style.buttonWrapper}>
-        <Button
-          as="a"
-          href=""
-          variant="primary"
-        >
+        <Button type="submit" variant="primary">
           Enviar consulta
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -106,5 +168,6 @@ const Form = () => {
     </form>
   );
 };
+
 
 export default Form;
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -15,33 +15,33 @@ const Services = () => {
     <div>
       <Banner data={selectedBanner} />
       <div className={style.contentWrapper}>
-      {isMobile ? (
-          <div className={`${style["pt-60"]} ${style["px-16"]}`}>
-            <h3 className="H3">
-              Encontrá el <span className="text-color-secondary">concesionario oficial</span> de Ford y{" "}
-              <span className="text-color-secondary">puntos de servicio multimarca</span> que más te convenga.
-            </h3>
-            <p className="text-color-neutral-500 subtitle-20">
-              Seleccioná el servicio de tu interés para conocer las sucursales:
-            </p>
-          </div>
-        ) : (
+        {isMobile ? (
             <div className={`${style["pt-60"]} ${style["px-16"]}`}>
-                <h3 className={`H3 ${style.textCenter}`}>
+              <h3 className="H3">
                 Encontrá el <span className="text-color-secondary">concesionario oficial</span> de Ford y{" "}
-                <span className="text-color-secondary">puntos</span>
-                <br />
-                <span className="text-color-secondary">de servicio multimarca</span> que más te convenga.
-                </h3>
-                <p className={`text-color-neutral-500 subtitle-20 ${style.textCenter}`}>
+                <span className="text-color-secondary">puntos de servicio multimarca</span> que más te convenga.
+              </h3>
+              <p className="text-color-neutral-500 subtitle-20">
                 Seleccioná el servicio de tu interés para conocer las sucursales:
-                </p>
+              </p>
             </div>
-        )}
+          ) : (
+              <div className={`${style["pt-60"]} ${style["px-16"]}`}>
+                  <h3 className={`H3 ${style.textCenter}`}>
+                  Encontrá el <span className="text-color-secondary">concesionario oficial</span> de Ford y{" "}
+                  <span className="text-color-secondary">puntos</span>
+                  <br />
+                  <span className="text-color-secondary">de servicio multimarca</span> que más te convenga.
+                  </h3>
+                  <p className={`text-color-neutral-500 subtitle-20 ${style.textCenter}`}>
+                  Seleccioná el servicio de tu interés para conocer las sucursales:
+                  </p>
+              </div>
+          )}
 
-        <div className={`${style["pt-42"]} ${style["px-16"]}`}>
-          <LocationSlider />
-        </div>
+          <div className={`${style["pt-42"]} ${style["px-16"]}`}>
+            <LocationSlider />
+          </div>
 
         {isMobile ? (
           <div className={style.wraper}>
@@ -56,8 +56,13 @@ const Services = () => {
             <Form />
           </div>
         ) : (
-          <div className={style.wraper}>
-            <h3 className="H3 text-color-dark">¿Necesitás asesoramiento?</h3>
+         <div className={style.wraper}>
+            <h3 className={`H3 text-color-dark ${style.mb0} `}>
+              ¿Necesitás <span className="text-color-secondary">asesoramiento?</span>
+            </h3>
+            <p className={`body-1-16 text-color-neutral-500 ${style.mt8}`}>
+              Completá los datos y nos pondremos en contacto a la brevedad.
+            </p>
             <Form />
           </div>
         )}
