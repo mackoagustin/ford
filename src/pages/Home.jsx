1fc6d2aeeb191eca920c587775a861d0babdcc2a
Agustin Macko <mackoaa@gmail.com>
2025-05-23 13:18:20 -0300

    fix banner double buton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 // src/pages/Home.jsx
 import React from 'react';
+import Footer from '../components/Footer/Footer';
 
 function Home() {
-  return <h1>HOME</h1>;
+  return  (
+    <div>
+        <h1>HOME</h1>;
+
+        <Footer />
+    </div>
+    
+  )
+  
+ 
+  
 }
 
 export default Home;
