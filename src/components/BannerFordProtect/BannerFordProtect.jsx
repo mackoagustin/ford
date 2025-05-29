abf671a8bb69111d10438578e9c0c2af8ba3d049
Agustin Macko <mackoaa@gmail.com>
2025-05-29 17:46:00 -0300

    Ford protect  page 50%

---
+++ src/components/BannerFordProtect/BannerFordProtect.jsx
@@ -0,0 +1,107 @@
+// src/components/Banner/Banner.jsx
+import React from "react";
+import styles from "./BannerFordProtect.module.css";
+import Button from "../Button/Button";
+import useIsMobile from "../../hook/useIsMobile";
+
+const BannerFordProtect = ({ data }) => {
+  if (!data) {
+    return <div>No data available</div>;
+  }
+
+  const isMobile = useIsMobile();
+  return (
+    <> 
+      {isMobile ? (
+        <div className={styles.banner}>
+          <img
+            src={data.image}
+            alt={data.title}
+            className={styles.backgroundImage}
+            loading="lazy"
+          />
+
+          <div className={styles.overlay} />
+
+          <div className={styles.content}>
+            <div className={styles.textGroup}>
+              <h1 className={`${styles.title}`}>
+                {data.title}
+              </h1>
+              <p className={styles.description}>{data.description}</p>
+              <Button
+                as="a"
+                href={data.ctaLink}
+                variant="primaryFull"
+              >
+                {data.ctaText}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2.4}
+                  stroke="currentColor"
+                  fill="none"
+                  className={styles["button-icon"]}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 4.5L15.75 12 8.25 19.5"
+                  />
+                </svg>
+              </Button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.banner}>
+        <img
+          src={data.image}
+          alt={data.title}
+          className={styles.backgroundImage}
+          loading="lazy"
+        />
+
+        <div className={styles.overlay} />
+
+        <div className={styles.content}>
+          <div className={styles.textGroup}>
+            <h1 className={`${styles.title}`}>
+              {data.title}
+            </h1>
+            <p className={`${styles.description} H3`}>{data.description}</p>
+            <Button
+              as="a"
+              target="_blank"
+              href={data.ctaLink}
+              variant="primary"
+            >
+              {data.ctaText}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                strokeWidth={2.4}
+                stroke="currentColor"
+                fill="none"
+                className={styles["button-icon"]}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 4.5L15.75 12 8.25 19.5"
+                />
+              </svg>
+            </Button>
+          </div>
+        </div>
+      </div>
+      )}
+    </> // <--- Close the React Fragment here
+  );
+};
+
+export default BannerFordProtect;
--- src/pages/postsale/FordProtect.jsx
+++ src/pages/postsale/FordProtect.jsx
@@ -1,11 +1,46 @@
 // src/pages/Home.jsx
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
+import BannerFordProtect from '../../components/BannerFordProtect/BannerFordProtect';
+import bannerData from '../../data/banners.json';
+import useIsMobile from '../../hook/useIsMobile';
+import styles from './FordProtect.module.css';
 
 function FordProtect() {
+  const selectedBanner = bannerData.banners[6];
+  const isMobile = useIsMobile();
   return (
     <div>
-      <h1>FordProtect</h1>
+      <BannerFordProtect data={selectedBanner} />
+
+      {isMobile ? (
+        <>
+           <div className={`${styles["pt-60"]} ${styles["px-16"]}`}>
+              <h3 className="H2 text-color-dark">
+              <span className='text-color-secondary'>Garantía extendida</span> para tu vehículo
+              </h3>
+              <p className="text-color-neutral-500 subtitle-20">
+              Contá con la tranquilidad de extender la garantía de tu Ford hasta el quinto año desde la compra, sin límite de kilometraje. Ford Protect está diseñado exclusivamente para vehículos en período de garantía y se adquiere en concesionarios oficiales Ford.
+              </p>
+            </div>
+        </>
+      ) : (
+       <>
+        <div className={`${styles["pt-60"]} ${styles["px-16"]}`}>
+          <div className={styles.wraperText}>
+            <h3 className="H2 text-color-dark text-center"> 
+              <span className='text-color-secondary'>Garantía extendida</span> para tu vehículo
+            </h3>
+              
+            <p className="text-color-neutral-500 subtitle-20 text-center">
+              Contá con la tranquilidad de extender la garantía de tu Ford hasta el quinto año desde la compra, sin límite de kilometraje. Ford Protect está diseñado exclusivamente para vehículos en período de garantía y se adquiere en concesionarios oficiales Ford.
+            </p>
+          </div>
+        </div>
+             
+          
+       </>
+      )}
       <Footer />
     </div>
   );
