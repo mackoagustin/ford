15bc3caa94a4af5448891e2aadd1ff647c189635
Agustin Macko <mackoaa@gmail.com>
2025-06-04 09:29:22 -0300

    faqs

--- src/pages/postsale/FAQ.jsx
+++ src/pages/postsale/FAQ.jsx
@@ -1,13 +1,49 @@
 // src/pages/Home.jsx
 import React from 'react';
+import styles from './FAQ.module.css';
 import Footer from '../../components/Footer/Footer';
+import bannerData from '../../data/banners.json';
+import BannerAdjudicationActs from '../../components/BannerAdjudicationActs/BannerAdjudicationsActs';
+import Accordion from '../../components/Accordion/Accordion';
+import faqData from '../../data/faq.json';
+import useIsMobile from '../../hook/useIsMobile';
 
 function FAQ() {
+  const selectedBanner = bannerData.banners[16];
+  const faq = faqData.faq;
+  const isMobile = useIsMobile();
+
   return (
-    <div>
-      <h1>FAQ</h1>
-      <Footer />
-    </div>
+    <>
+      <BannerAdjudicationActs data={selectedBanner} />
+      {isMobile ? (
+        <div className={`${styles["pt-60"]} ${styles["px-16"]}`}>
+          <p className={`${styles["pt-60"]} ${styles["px-16"]} H2 text-color-dark`}>Preguntas frecuentes</p>
+        </div>
+      ) : (
+        <div className={`${styles["pt-60"]} ${styles["px-16"]}`}>
+          <div className={styles.wraperText}>
+          <p className={`${styles["pt-60"]} ${styles["px-16"]} H2 text-color-dark`}>Preguntas frecuentes</p>
+          </div>
+        </div>
+      )}
+
+
+      <div className={styles.wraperAccordion}>
+      {faq.map((item, index) =>(
+        <Accordion
+        key={index}
+        question={item.question}
+        answer={item.answer}
+        variant={index === 0 ? 'parent' : 'child'}
+        />
+      ))}
+      </div>
+   
+
+
+    <Footer />
+    </>
   );
 }
 
